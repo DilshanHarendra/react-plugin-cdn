2fffb6f9814c8d0d1fcaf1b39351ad087731804d
DilshanHarendra <harendracripto@gmail.com>
2024-03-30 21:22:11 +0530

    create main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {init} from "./index";
-init()
-ReactDOM.createRoot(document.getElementById('app-root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
